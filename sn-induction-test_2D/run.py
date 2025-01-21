d337725b3271fc12a21bf512a7a260a8db9cc8cb
Arved Wintzer <arved.wintzer@posteo.de>
2025-01-21 21:11:57 +0100

    add Sepehr's source code for energy balace manuscript

---
+++ sn-induction-test_2D/run.py
@@ -0,0 +1,23 @@
+import opencgs.control as ctrl
+from opencgs.sim import DiameterIteration, ParameterStudy, SteadyStateSim
+from setup import geometry, simulation
+
+
+if __name__ == "__main__":
+    try:
+        git_metadata = ctrl.get_git_metadata()
+    except:
+        git_metadata = "not available"  # no git on cluster nodes
+
+    geo_config = ctrl.load_config("./config_geo.yml")
+    sim_config = ctrl.load_config("./config_sim.yml")
+    mat_config = ctrl.load_config("./config_mat.yml")
+    config = ctrl.load_config("./config.yml")
+    config.update({"metadata": git_metadata})
+
+    if "study_params" in config:
+        sim = ParameterStudy(SteadyStateSim, geometry, geo_config, simulation, sim_config, mat_config, **config)
+    else:
+        sim = SteadyStateSim(geometry, geo_config, simulation, sim_config, mat_config, **config)
+
+    sim.execute()
